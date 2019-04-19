9e3b04184713c1edbf23af1fef0df8adb6a69163
envdata <apshivkumar1@gmail.com>
2019-04-18 20:17:21 -0400

    added loop to extract data

--- FinData_screener.R
+++ FinData_screener.R
@@ -2,31 +2,24 @@
 
 require(RCurl)
 require(jsonlite)
-library(Quandl)
+library(rvest)
+library(httr)
 
-myticker<-"XNSE:3MINDIA"
-##url.histprice<-function(x){ return(paste0("http://globalquote.morningstar.com/globalcomponent/RealtimeHistoricalStockData.ashx?ticker=",x,"&showVol=true&dtype=his&f=d&curry=USD&range=1900-1-1|2014-10-10&isD=true&isS=true&hasF=true&ProdCode=DIRECT"))}
-url.keyratios<-function(x){return(paste0("http://financials.morningstar.com/ajax/exportKR2CSV.html?t=",x))}
 
-#Prices from Quandl
+
+url.screener<-"https://www.screener.in/company/"
+
+#NSE codes scrips whatever you call it
 NSE_CODES=read.csv("C:/Users/ashivkumar/Documents/temp/Finfun/NSE_Codes.csv",header=TRUE, stringsAsFactors = FALSE)
+NSE_symbols_only=NSE_CODES$Symbol[1:10]
+#colnames(NSE_CODES)=c("Codes","Company")
 
-colnames(NSE_CODES)=c("Codes","Company")
-testprice=Quandl("NSE/3MINDIA", api_key="uYHynQoeChxswuz2Sz6g")
 
-##Import prices from Quandl
-ls_codes=list()
-##testing with 10 
-for(pr in NSE_CODES[1:10,1]){
-    ls_codes[[which(NSE_CODES==pr)]]=Quandl(pr, api_key="uYHynQoeChxswuz2Sz6g")
-  } 
-
-index(NSE_CODES[1,1])
-get_KeyRatios=function(){
+get_incomestat=function(){
 NSE500_fund=list()
 NSE_symbols_only=NSE_CODES$Symbol[10:20]
 for(t in 1:length(NSE_symbols_only)){
-  myticker=paste0("XNSE:",NSE_symbols_only[t])
+  myticker=NSE_symbols_only[t]
  
 # Fundamentals 
 str.keyratios<-getURL("https://financials.morningstar.com/finan/ajax/exportKR2CSV.html?&callback=?&t=0P0000BP80&region=ind&culture=en-US&version=SAL&cur=&order=asc"); 
@@ -57,14 +50,28 @@ return(NSE500_fund)
 
 
 
-si=html_session("https://www.screener.in/company/3MINDIA/")
-url="https://www.screener.in/company/3MINDIA/"
+#si=html_session("https://www.screener.in/company/3MINDIA/")
 
-p=url %>%
+myticker <- "3MINIDIA"
+for(t in 1:length(NSE_symbols_only)){
+  myticker=NSE_symbols_only[t]
+  url=paste0(url.screener, myticker)
+  
+  table_incomestat[[myticker]]=url %>%
   read_html() %>%
   html_nodes(xpath='//*[@id="profit-loss"]/div[1]/table')%>%
   html_table(.,fill=TRUE)
+Sys.sleep(10)
+}
+t=data.frame(table_incomestat[[12]])
+rownames(t)<- t[1:12,1]; t<-t[,-1];
+t[1:12,1:13]= as.numeric(t[1:12,1:13])
 
+tt<- transform(t, t[1:12,] = as.numeric(t[1:12,]))
+
+as.numeric(t[1,1])
+
+tt=data.frame(lapply(t, function(x) { gsub("%","",gsub(",","",x))}))
 
 
 #Retrieve historical prices
