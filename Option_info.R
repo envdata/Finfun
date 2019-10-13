2b0612e0da5d932af1033f7fa3da013592e8b857
envdata <apshivkumar1@gmail.com>
2019-10-13 01:26:08 -0400

    n o changes

---
+++ Option_info.R
@@ -0,0 +1,54 @@
+# Scrape NSE website to extract option chain data 
+
+
+#libraries 
+library(rvest)
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(robotstxt)
+library(dplyr)
+
+#Check for permissions
+
+paths_allowed(paths=c("https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-10000&symbol=NIFTY&symbol=NIFTY&instrument=-&date=-&segmentLink=17&symbolCount=2&segmentLink=17"))
+
+test.url <-  "https://www.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-10000&symbol=NIFTY&symbol=NIFTY&instrument=-&date=-&segmentLink=17&symbolCount=2&segmentLink=17"
+
+option_table=list()
+
+option_table <-  test.url %>%
+  read_html() %>%
+  html_nodes(xpath='//*[@id="octable"]')%>%
+  html_table(.,fill=TRUE)
+
+#option_table <- lapply(option_table, function(x) { gsub(",","",x)})
+
+
+#Split calls and puts and transform data for analysis 
+split_and_transform = function(option_table){
+  option_call_only=option_table[[1]][names(option_table[[1]]) == "CALLS"] #just calls
+  colnames(option_call_only)=option_call_only[1,]
+  option_call_only <-  option_call_only[-1,-1]
+  option_call_only <-  data.frame(option_call_only)
+  option_call_only <- data.frame(lapply(option_call_only,function(x) as.numeric(gsub(",","",x))))
+  #option_call_only <- mutate_all(option_call_only, function(x) as.numeric(x))
+  option_call_only$option = "CALLS"
+  
+  option_put_only=option_table[[1]][names(option_table[[1]]) == "PUTS"] #just PUTS
+  colnames(option_put_only)=option_put_only[1,]
+  option_put_only <-  option_put_only[-1,-11]
+  option_put_only <-  data.frame(option_put_only)
+  option_put_only <- data.frame(lapply(option_put_only, function(x) as.numeric(gsub(",","",x))))
+  #option_put_only <- mutate_all(option_put_only, function(x) as.numeric(x))
+  option_put_only <- option_put_only[,c(10,9,8,7,6,5,1,2,3,4)]  
+  option_put_only$option = "PUTS"
+  colnames(option_put_only) <-  colnames(option_call_only)
+  
+  #combine calls and puts
+  option_call_only$'Strike' <- option_table[[1]][[12]][2:81]
+  option_transform_tbl <-  cbind(option_call_only,option_put_only)
+
+}
+
+
